52276c2577e8ebd0e81cd4cd8240a382cd8d1a40
nomansadiq11 <nomi.cs786@gmail.com>
2020-01-13 19:38:26 +0500

    try catch and vsts variable added

--- powershell.ps1
+++ powershell.ps1
@@ -1,7 +1,26 @@
 
+[CmdletBinding()] 
+param() 
+ 
+Trace-VstsEnteringInvocation $MyInvocation 
+try { 
+    
+    
+    # Get inputs. 
+    $ServiceName = Get-VstsInput -Name 'ServiceName' -Require 
+    $StartorSttop = Get-VstsInput -Name 'StartorSttop' -Require 
+
+    Get-Service "SQL Server (NS2014)" | Where {$_.status –eq 'Running'} |  Stop-Service
 
 
-Get-Service "SQL Server (NS2014)" | Where {$_.status –eq 'Running'} |  Stop-Service
+    # Get-Service "SQL Server (NS2014)" | Where {$_.status –eq 'Stopped'} |  Start-Service
 
 
-Get-Service "SQL Server (NS2014)" | Where {$_.status –eq 'Stopped'} |  Start-Service
+    # Set-Service "SQL Server (NS2014)" -StartupType manual
+     
+
+} finally { 
+    Trace-VstsLeavingInvocation $MyInvocation 
+}
+
+
